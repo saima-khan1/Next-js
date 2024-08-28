2227d291a4c152503b7c37d0b6ce5dd3dc0887a6
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2024-08-28 10:11:08 +0200

    crud operation with dummy data

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
+      <Link href="users">Users </Link>
     </main>
   );
 }
